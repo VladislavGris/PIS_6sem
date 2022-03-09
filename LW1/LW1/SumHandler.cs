c672e6e0d095f62d8459a16e215d5d5699cdceb0
Vladislav Grishkevich <vladislav.grishkevich@gmail.com>
2022-03-09 22:00:13 +0300

    Lab 1.POST Sum handler

---
+++ LW1/LW1/SumHandler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Web;
+
+namespace LW1
+{
+    public class SumHandler : IHttpHandler
+    {
+        private static string ParmAName = "X";
+        private static string ParmBName = "Y";
+
+        public bool IsReusable
+        {
+            get { return true; }
+        }
+
+        public void ProcessRequest(HttpContext context)
+        {
+            string paramA = context.Request.QueryString[ParmAName];
+            string paramB = context.Request.QueryString[ParmBName];
+            float X, Y;
+            HttpResponse response = context.Response;
+            if (!float.TryParse(paramA, out X) || !float.TryParse(paramB, out Y))
+            {
+                response.Write("X and/or Y is not a number");
+            }
+            else
+            {
+                response.Write(X+Y);
+            }
+        }
+    }
+}
