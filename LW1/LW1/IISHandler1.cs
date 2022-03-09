50b817759d85267d98457f8e85fce1ae34dbbc13
Vladislav Grishkevich <vladislav.grishkevich@gmail.com>
2022-03-09 08:45:13 +0300

    Lab 1. HttpHandler

---
+++ LW1/LW1/IISHandler1.cs
@@ -0,0 +1,31 @@
+﻿using System.Web;
+
+namespace LW1
+{
+    public class IISHandler1 : IHttpHandler
+    {
+        private static string ParmAName = "ParmA";
+        private static string ParmBName = "ParmB";
+
+        public bool IsReusable
+        {
+            get { return true; }
+        }
+
+        public void ProcessRequest(HttpContext context)
+        {
+            string paramA = context.Request.QueryString[ParmAName];
+            string paramB = context.Request.QueryString[ParmBName];
+            HttpResponse response = context.Response;
+            if(string.IsNullOrEmpty(paramA) || string.IsNullOrEmpty(paramB))
+            {
+                response.Write("ParmA and/or ParmB is null or empty");
+            }
+            else
+            {
+                response.Write($"GVA:ParmA = {paramA},ParmB = {paramB}");
+            }
+            
+        }
+    }
+}
